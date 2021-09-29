64e3591c32eb51aee46521096b93340e8e6ab597
Nguyễn Huỳnh Việt Tiến <84427622+t13n8818@users.noreply.github.com>
2021-09-29 13:12:22 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaBooking.Areas.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Admin/Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
