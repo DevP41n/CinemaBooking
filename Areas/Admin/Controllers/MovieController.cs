51f9d9584a18c8e369be14d7728211cdba28af3d
Nguyễn Huỳnh Việt Tiến <84427622+t13n8818@users.noreply.github.com>
2021-09-30 22:39:20 +0700

    Tạo Movie Controller làm chung với Tân

---
+++ Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using CinemaBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaBooking.Areas.Admin.Controllers
+{
+    public class MovieController : Controller
+    {
+        private CinemaBookingEntities db = new CinemaBookingEntities();
+        // GET: Admin/Movie
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
