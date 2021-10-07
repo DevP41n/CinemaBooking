eafbaffe44658cd9c27282ad96524c0c6838a0cd
Nguyễn Huỳnh Việt Tiến <84427622+t13n8818@users.noreply.github.com>
2021-10-07 20:35:28 +0700

    Login Admin (fix noti)

--- Areas/Admin/Controllers/AuthController.cs
+++ Areas/Admin/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CinemaBooking.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,55 @@ namespace CinemaBooking.Areas.Admin.Controllers
 {
     public class AuthController : Controller
     {
+        private CinemaBookingEntities db = new CinemaBookingEntities();
         // GET: Admin/Auth
         public ActionResult Login()
         {
+            if (Session["HoTen"] != null)
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var f_password = MyString.ToMD5(password);
+                var data = db.users.Where(s => s.username.Equals(username) && s.password.Equals(f_password)).ToList();
+
+                if (data.Count() > 0)
+                {
+
+                    //add session
+                    Session["HoTen"] = data.FirstOrDefault().ho_ten;
+                    Session["Username"] = data.FirstOrDefault().username;
+                    Session["Id"] = data.FirstOrDefault().id;
+                    var ten = Convert.ToString(Session["HoTen"]);
+                    TempData["Message"] = "Đăng nhập thành công!</br>" + "Xin chào " + ten;
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+                else
+                {
+                    TempData["Warning"] = "Sai Thông tin đăng nhập vui lòng nhập lại!";
+                    return RedirectToAction("Login");
+                }
+            }
             return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
         }
     }
 }
