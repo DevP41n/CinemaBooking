5b2661b8508b4cf9d07a1c90ae851328490e0b58
PhuocNguyenG <73028315+PhuocNguyenG@users.noreply.github.com>
2021-10-31 19:51:14 +0700

    update sql

--- Models/CinemaBooking.Context.cs
+++ Models/CinemaBooking.Context.cs
@@ -33,6 +33,7 @@ namespace CinemaBooking.Models
         public virtual DbSet<lien_he> lien_he { get; set; }
         public virtual DbSet<list_phim_dienvien> list_phim_dienvien { get; set; }
         public virtual DbSet<loai_ghe> loai_ghe { get; set; }
+        public virtual DbSet<movie_rate> movie_rate { get; set; }
         public virtual DbSet<phim> phims { get; set; }
         public virtual DbSet<phong_chieu> phong_chieu { get; set; }
         public virtual DbSet<su_kien> su_kien { get; set; }
@@ -41,6 +42,5 @@ namespace CinemaBooking.Models
         public virtual DbSet<thong_tin_cong_ty> thong_tin_cong_ty { get; set; }
         public virtual DbSet<user> users { get; set; }
         public virtual DbSet<ve_ban> ve_ban { get; set; }
-        public virtual DbSet<movie_rate> movie_rate { get; set; }
     }
 }
--- Models/khach_hang.cs
+++ Models/khach_hang.cs
@@ -11,7 +11,8 @@ namespace CinemaBooking.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.Linq;
+
     public partial class khach_hang
     {
         CinemaBookingEntities db = new CinemaBookingEntities();
