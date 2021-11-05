3a4ae7f2153772ee48e719447065bf8573357450
Nguyễn Huỳnh Việt Tiến <91588221+DevP41n@users.noreply.github.com>
2021-11-05 15:34:06 +0700

    fixxxxxxxx

--- Database/Database-Cinema-Booking.sql
+++ Database/Database-Cinema-Booking.sql
@@ -412,3 +412,15 @@ drop table dbo.phim_daodien;
 alter table dbo.phim add idrating nvarchar(50) NULL 
 --Đẻ dánh giá nội dung có phù hợp với độ tuổi
 alter table dbo.phim add id_content_rating int NULL
+
+
+alter table phim
+add constraint Fk_DaoDien foreign key(dao_dien_id) references dao_dien(id)
+
+alter table phim
+drop constraint Fk_TheLoai 
+
+alter table phim
+drop column dien_vien_id;
+alter table phim
+drop column the_loai_phim_id;
