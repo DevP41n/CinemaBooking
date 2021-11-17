72d309a21df5e25ec4b35bd94bc7337425e33b44
sineemxc <76941616+sineemxc@users.noreply.github.com>
2021-11-17 23:59:56 +0700

    Order list v1.

---
+++ Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using CinemaBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CinemaBooking.Areas.Admin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private CinemaBookingEntities db = new CinemaBookingEntities();
+        // GET: Admin/Orders
+        public ActionResult ListOrders()
+        {
+            var order = db.orders.OrderByDescending(n => n.id);
+
+
+            return View(order);
+        }
+
+        public ActionResult OrderDetail(int? idorder)
+        {
+            var detail = db.order_details.Where(n => n.id_orders == idorder).ToList();
+            var Count = detail.Count();
+            List<String> top = new List<String>();
+            List<String> tien = new List<String>();
+            foreach (var item in detail)
+            {
+                top.Add(item.ghe_ngoi.Row + item.ghe_ngoi.Col);
+                tien.Add(item.gia_ve.ToString());
+            }
+            return Json(data: new { Count, top, tien }, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
