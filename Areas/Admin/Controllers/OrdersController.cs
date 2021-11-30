a32ca8d77e59f571c5c2377db96b7ef374169ead
Nguyễn Huỳnh Việt Tiến <91588221+DevP41n@users.noreply.github.com>
2021-11-30 23:50:12 +0700

    fix ràng buộc tiếp

--- Areas/Admin/Controllers/OrdersController.cs
+++ Areas/Admin/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using CinemaBooking.Models;
 using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -11,6 +12,18 @@ namespace CinemaBooking.Areas.Admin.Controllers
         // GET: Admin/Orders
         public ActionResult ListOrders()
         {
+            TimeSpan tinhgio = new TimeSpan(0, 15, 0); // 15 phút
+            //Status 2: đang chờ thanh toán tại quầy
+            var orderss = db.orders.Where(n => n.status == 2);
+            foreach (var itemm in orderss)
+            {
+                if (itemm.ngay_mua + tinhgio <= DateTime.Now)
+                {
+                    itemm.status = 0;
+                    db.Entry(itemm).State = EntityState.Modified;
+                }
+            }
+            db.SaveChanges();
             return View(db.orders.OrderByDescending(n => n.ngay_mua).ToList());
         }
 
